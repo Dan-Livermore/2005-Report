6e28d7713ee78fa2fcbab053ac9ec541d94a02f5
(s) Daniel Livermore <djlivermore@socnet.soc.plymouth.ac.uk>
2023-05-02 16:33:46 +0100

    Inital Java, Grade, Springboot commit

---
+++ src/test/java/com/example/COMP2005Report/Comp2005ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.COMP2005Report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp2005ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
