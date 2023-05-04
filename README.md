003adc21cf4be5454a26a15de14a51ee2b17a654
(s) Daniel Livermore <djlivermore@SOCNET.SOC.PLYMOUTH.AC.UK>
2023-05-04 15:04:21 +0100

    Allocation and Admission endpoints get all data, attempted parsing the json into objects in the employees one

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Repo for COMP 2005 Maternity API System
+
+Unit tests
+- 200 connection
+- whether the url is correct
+- json in correct form
+- 404?
--- src/main/java/com/example/COMP2005Report/GetAllocationController.java
+++ src/main/java/com/example/COMP2005Report/GetAllocationController.java
@@ -1,31 +1,21 @@
 package com.example.COMP2005Report;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
-public class GetEmployeesController {
-    public class Employee{
-        private static int id;
-        private static String surname;
-        private static String forename;
-    }
-
-    //HTTP URL Connection
+public class GetAllocationController {
     private static HttpURLConnection connection;
-
     public static void main(String[] args){
+        //HTTP URL Connection
         BufferedReader reader;
         String line;
         StringBuffer responseContent = new StringBuffer();
         try {
-            URL url = new URL("https://web.socem.plymouth.ac.uk/COMP2005/api/Employees");
+            URL url = new URL("https://web.socem.plymouth.ac.uk/COMP2005/api/Allocations");
             connection = (HttpURLConnection) url.openConnection();
 
             connection.setRequestMethod("GET");
@@ -53,4 +43,3 @@ public class GetEmployeesController {
         }
     }
 }
-
